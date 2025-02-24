417558d6f250a41e3d3d1f896dedfb0f39e26dff
sarvani8179 <sarvani8179@gmail.com>
2025-02-24 13:43:52 +0530

    adding files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Parentimg from './components/parentimg';
+
+function App() {
+  return (
+    <div className="App">
+      <Parentimg/>
+    </div>
+  );
+}
+
+export default App;
